5eabc25c26b7d7627d9d2599bf7a9c75d259929f
Andrii Lypovetskyi <li_andr@ukr.net>
2021-07-20 23:15:34 +0300

    Добавил меню по шаблону

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,1 +1,10 @@
+import menu from '../menu.json';
+import menuTemplate from '../templates/menu.hbs';
 console.log("menu");
+console.log(menu);
+
+ const menuRef = document.querySelector('.js-menu');
+
+
+ const markup = menuTemplate(menu);
+ menuRef.insertAdjacentHTML('beforeend', markup);
