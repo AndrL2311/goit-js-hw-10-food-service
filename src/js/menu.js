67200ae141fef8ffc89dd3f678fdf82f1977eafd
Andrii Lypovetskyi <li_andr@ukr.net>
2021-07-21 10:27:37 +0300

    Update menu.js

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,10 +1,8 @@
 import menu from '../menu.json';
 import menuTemplate from '../templates/menu.hbs';
-console.log("menu");
-console.log(menu);
 
  const menuRef = document.querySelector('.js-menu');
 
-
- const markup = menuTemplate(menu);
+const markup = menuTemplate(menu);
+ 
  menuRef.insertAdjacentHTML('beforeend', markup);
